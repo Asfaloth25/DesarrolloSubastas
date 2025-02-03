9c16e8c768c7e550bdeef786085a9b80d098b49f
Asfaloth25 <118351449+Asfaloth25@users.noreply.github.com>
2025-02-03 16:15:05 +0100

    fixed sign in js

--- pedro.js
+++ pedro.js
@@ -1,5 +1,15 @@
-function validatePasswords(){
-    const password = document.getElementById('pword').value;
-}
+document.getElementById("sign_in_form").addEventListener("submit", function (event) {
+    const password = document.getElementById("pword").value;
+    const confirmPassword = document.getElementById("confirmpword").value;
 
+    const errorMessage = document.getElementById("error_message");
 
+    if (password !== confirmPassword) {
+        event.preventDefault();
+        errorMessage.style.display = "block";
+        errorMessage.textContent = "Las contraseñas no son iguales";
+    } else {
+        errorMessage.style.display = "none";
+        alert("Las contraseñas coinciden. Redirigiendo a la página de inicio de sesión...");
+    }
+});
